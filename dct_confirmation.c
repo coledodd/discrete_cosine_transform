1acdfece5330a8734499622469cc71d84b862a46
root <root@localhost.localdomain>
2021-08-11 16:36:25 -0700

    finished up working dct code, still needs to be tested

--- dct_confirmation.c
+++ dct_confirmation.c
@@ -1,10 +1,11 @@
 #include <math.h>
+#include <stdio.h>
 //=================PROTOTYPES==============================
 void dct(float[8][8]);
 float X(int,int);
 float C(int);
 
-float input**;
+float (*input)[8];
 
 float C(int u){
   return (u == 0)? 1.0/sqrt(2):1;
@@ -14,7 +15,7 @@ float X(int u, int v){
   float sum = 0;
   for(int i = 0; i < 7; i++){
     for(int j = 0; j < 7; j++){
-      sum = input[i][j] * cos(2*j+1)*PI*u/16 * cos(2*j+1)*PI*v/16;
+      sum = input[i][j] * cos((double)(2*j+1))*M_PI*u/16 * cos((double)(2*j+1))*M_PI*v/16;
     }
   }  
   return sum * (C(u)/2) * (C(v)/2);
@@ -29,5 +30,42 @@ void dct(float a[8][8]){
        result[i][j] = X(i,j);
     }
   }
+  
+  for(int i = 0; i < 7; i++){
+    for(int j = 0; j < 7; j++){
+      a[i][j]=result[i][j];
+    }
+  }
+}
+
+int main(){
+  float image[8][8] = {
+	{1,2,3,4,5,6,7,8},
+	{1,2,3,4,5,6,7,8},
+	{1,2,3,4,5,6,7,8},
+	{1,2,3,4,5,6,7,8},
+	{1,2,3,4,5,6,7,8},
+	{1,2,3,4,5,6,7,8},
+	{1,2,3,4,5,6,7,8},
+	{1,2,3,4,5,6,7,8}
+  };
+  
+  printf("original input:\n"); 
+  for(int i = 0; i < 7; i++){
+    for(int j = 0; j < 7; j++){
+      printf("%f ",image[i][j]);
+    }
+    printf("\n");
+  }
+
+  dct(image);
+  
+  printf("\n\nafter DCT:\n");
+  for(int i = 0; i < 7; i++){
+    for(int j = 0; j < 7; j++){
+      printf("%f ",image[i][j]);
+    }
+    printf("\n");
+  }  
 
 }
