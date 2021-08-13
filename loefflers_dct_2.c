5548b71c24647196cf3b47cb969838d5fed878a9
Tristan Cusi <cusitristan@gmail.com>
2021-08-12 20:40:46 -0700

    fixed bugs

--- loefflers_dct_2.c
+++ loefflers_dct_2.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 #define reflector(a, b) tmp0=a; a=tmp0+b; b=tmp0-b;
-#define rotator(a, b, c0, c1, c2) a<<=4; b<<=4; tmp0 = c1*b + c0*(a+b); b = c1*b + c0*(a+b); a=tmp0; a>>=4; b>>=4;
-#define scale(a) a<<=4; a*6; a>>=4;
+#define rotator(a, b, c0, c1, c2) tmp0 = c1*b + c0*(a+b); b = c2*b + c0*(a+b); a=tmp0; a>>=4; b>>=4;
+#define scale(a) a*=23; a>>=4;
 
 void loeffler(int8_t arr[8]) {
 	int32_t load3210 = ((int32_t*) arr)[0];
@@ -32,7 +32,6 @@ void loeffler(int8_t arr[8]) {
 	//stage 4
 	
 	//assignment
-	x0 >>= 8; x1 >>= 8; x2 >>= 8; x3 >>= 8;
 	store3210 |= x0;
 	store3210 |= x2<<16;
 	store7654 |= x1;
@@ -40,10 +39,11 @@ void loeffler(int8_t arr[8]) {
 	
 	//odd
 	//stage 1
-	x0 = ((load3210&0x000000ff)>>0) - ((load7654&0xff000000)>>24);
-	x1 = ((load3210&0x0000ff00)>>8) - ((load7654&0x00ff0000)>>16);
-	x2 = ((load3210&0x00ff0000)>>16) - ((load7654&0x0000ff00)>>8);
-	x3 = ((load3210&0xff000000)>>24) - ((load7654&0x000000ff)>>0);
+	//reusing registers here
+	x3 = ((load3210&0x000000ff)>>0) - ((load7654&0xff000000)>>24);
+	x2 = ((load3210&0x0000ff00)>>8) - ((load7654&0x00ff0000)>>16);
+	x1 = ((load3210&0x00ff0000)>>16) - ((load7654&0x0000ff00)>>8);
+	x0 = ((load3210&0xff000000)>>24) - ((load7654&0x000000ff)>>0);
 	
 	//stage 2
 	rotator(x0,x3,13,-4,-22);
@@ -58,7 +58,6 @@ void loeffler(int8_t arr[8]) {
 	scale(x1);
 	scale(x2);
 	
-	x0 >>= 8; x1 >>= 8; x2 >>= 8; x3 >>= 8;
 	store7654 |= x0<<24;
 	store3210 |= x1<<24;
 	store7654 |= x2<<8;
