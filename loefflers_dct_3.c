f077e96bd55b07dddb3a752d3cac6e728795812b
Cole Dodd <cole@ruboss.com>
2021-08-13 15:09:44 -0700

    fixed compilation erros

--- loefflers_dct_3.c
+++ loefflers_dct_3.c
@@ -5,7 +5,7 @@
 #define rotator(a, b, c0, c1, c2) tmp0 = c1*b + c0*(a+b); b = c2*b + c0*(a+b); a=tmp0; a = a & 0xf ? (a>>4) | 1 : a>>4; b = b & 0xf ? (b>>4) | 1 : b>>4;
 #define scale(a)a = a & 0xf ? (a>>4) | 1 : a>>4;
 
-void loeffler(int16_t x) {
+void loeffler(int16_t x[8]) {
 	int32_t x0 = x[0];
 	int32_t x1 = x[1];
 	int32_t x2 = x[2];
@@ -14,6 +14,8 @@ void loeffler(int16_t x) {
 	int32_t x5 = x[5];
 	int32_t x6 = x[6];
 	int32_t x7 = x[7];
+
+	int32_t tmp0;
 	
 	reflector(x0,x7);
 	reflector(x1,x6);
@@ -74,7 +76,7 @@ int main() {
 
 	for (int i = 0; i < 8; i++) {
 		for (int j = 0; j < 8; j++)
-			printf("%d, ", input[i][j] & 0b111 ? (input[i][j] >> 3) | 1) : (input[i][j] >> 3));
+			printf("%d, ", input[i][j] & 0b111 ? (input[i][j] >> 3) | 1 : (input[i][j] >> 3));
 		puts("\n");
 	}
 	return 0;
