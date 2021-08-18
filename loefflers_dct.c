b9257301018fd6c1ce9ad6321a5707745662d2f2
Cole Dodd <cole@ruboss.com>
2021-08-18 09:19:00 -0700

    optimized round 1

--- loefflers_dct.c
+++ loefflers_dct.c
@@ -1,76 +1,63 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define reflector(a, b) tmp0=a; a=tmp0+b; b=tmp0-b;
-#define rotator(a, b, c0, c1, c2) tmp0 = c1*b + c0*(a+b); b = c2*a + c0*(a+b); a=tmp0; a=round_16(a); b=round_16(b);
+#define reflector(a, b) { uint32_t tmp0=a; a=tmp0+b; b=tmp0-b; }
+#define rotator(a, b, c0, c1, c2) { uint32_t tmp0 = c1*b + c0*(a+b); b = c2*a + c0*(a+b); a=tmp0; a=round_16(a); b=round_16(b); }
 #define scale(a) a*=92682; a = round_16(a);
 #define round_16(a) ((a & 0x8000) ? ((a>>16) + 1) : (a>>16))
-#define round_3(a) ((a & 0b10000) ? ((a >> 5) + 1) : (a >> 5))
+#define round_5(a) ((a & 0b100) ? ((a >> 3) + 1) : (a >> 3))
 #define reflector_1(a,b) { uint32_t tmp = a; \
 						a = (tmp&0xffff0000) | ((int32_t)((int16_t)(tmp&0x0000ffff)) + (int32_t)((int16_t)(b>>16))); \
 						b = (b&0x0000ffff) | (((int32_t)((int16_t)(tmp&0x0000ffff)) - (int32_t)((int16_t)(b>>16))) << 16); }
 #define reflector_2(a,b) { uint32_t tmp = a; \
 						a = (tmp&0x0000ffff) | (((int32_t)((int16_t)(tmp>>16)) + (int32_t)((int16_t)(b&0x0000ffff))) << 16); \
 						b = (b&0xffff0000) | (((int32_t)((int16_t)(tmp>>16)) - (int32_t)((int16_t)(b&0x0000ffff)))&0xffff); }
-void dct_round1(register uint8_t x[240][320], register int16_t X[240][320]) {
+
+void dct_round1(uint8_t x_[240][320], int16_t X_[240][320]) {
+	register int32_t* x = (int32_t*)(x_[0]);
+	register int32_t* X = (int32_t*)(X_[0]);
+	// register int16_t* X = (X_[0]);
+
 	register int i;
-	register int j;
-	for (i = 0; i < 240; i++) {
-		for (j = 0; j < 320; j+=8) {
-			// int32_t x3210 = ((int32_t*)(x[i]))[j+0]; //load 0 to 3
-			// int32_t x7654 = ((int32_t*)(x[i]))[j+4]; //load 4 to 7
-			
-			// int32_t x0 = ((x3210>>0)&0xf)+((x7654>>24)&0xf);
-			// int32_t x7 = ((x3210>>0)&0xf)-((x7654>>24)&0xf);
-			// int32_t x1 = ((x3210>>8)&0xf)+((x7654>>16)&0xf);
-			// int32_t x6 = ((x3210>>8)&0xf)-((x7654>>16)&0xf);
-			// int32_t x2 = ((x3210>>16)&0xf)+((x7654>>8)&0xf);
-			// int32_t x5 = ((x3210>>16)&0xf)-((x7654>>8)&0xf);
-			// int32_t x3 = ((x3210>>24)&0xf)+((x7654>>0)&0xf);
-			// int32_t x4 = ((x3210>>24)&0xf)-((x7654>>0)&0xf);
+	for (i = 0; i < 76800; i+=8 ){
+			// register int32_t x0 = *(x++);
+			// register int32_t x1 = *(x++);
+			// register int32_t x2 = *(x++);
+			// register int32_t x3 = *(x++);
+			// register int32_t x4 = *(x++);
+			// register int32_t x5 = *(x++);
+			// register int32_t x6 = *(x++);
+			// register int32_t x7 = *(x++);
 
-			
-			/*********************/
+			int32_t r0, r1, r2, r3, r4, r5;
+			r4 = *((int32_t*) x); //3210
+			x++;
+			r5 = *((int32_t*) x); //7654
+			x++;
 
-			register int32_t x0 = x[i][j+0] << 2;
-			register int32_t x1 = x[i][j+1] << 2;
-			register int32_t x2 = x[i][j+2] << 2;
-			register int32_t x3 = x[i][j+3] << 2;
-			register int32_t x4 = x[i][j+4] << 2;
-			register int32_t x5 = x[i][j+5] << 2;
-			register int32_t x6 = x[i][j+6] << 2;
-			register int32_t x7 = x[i][j+7] << 2;
-			// register int32_t x0 = x[i][j+0];
-			// register int32_t x1 = x[i][j+1];
-			// register int32_t x2 = x[i][j+2];
-			// register int32_t x3 = x[i][j+3];
-			// register int32_t x4 = x[i][j+4];
-			// register int32_t x5 = x[i][j+5];
-			// register int32_t x6 = x[i][j+6];
-			// register int32_t x7 = x[i][j+7];
+			r0 = ((r4&(0x0000ff00))<<8) | ((r4&(0x000000ff)));
+			r1 = ((r4&(0xff000000))>>8) | ((r4&(0x00ff0000))>>16);
+			r2 = ((r5&(0x0000ff00))<<8) | ((r5&(0x000000ff)));
+			r3 = ((r5&(0xff000000))>>8) | ((r5&(0x00ff0000))>>16);
 
+			// printf("%d %d %d %d %d %d %d %d\n", r0&0x0000ffff, (r0&0xffff0000)>>16,r1&0x0000ffff, (r1&0xffff0000)>>16,r2&0x0000ffff, (r2&0xffff0000)>>16,r3&0x0000ffff, (r3&0xffff0000)>>16);
+			// return;
 
-			int32_t r3 = (x7<<16)|(x6&0xffff); //76
-			int32_t r2 = (x5<<16)|(x4&0xffff); //76
-			int32_t r1 = (x3<<16)|(x2&0xffff); //76
-			int32_t r0 = (x1<<16)|(x0&0xffff); //76
-			
-			
+			// int32_t r3 = (x7<<16)|(x6&0xffff); //76
+			// int32_t r2 = (x5<<16)|(x4&0xffff); //54
+			// int32_t r1 = (x3<<16)|(x2&0xffff); //32
+			// int32_t r0 = (x1<<16)|(x0&0xffff); //10
 
-			register int32_t tmp0;
 			reflector_1(r0, r3);
 			reflector_2(r0, r3);
 			reflector_1(r1, r2);
 			reflector_2(r1, r2);
 
-			//printf("try: %d %d %d %d %d %d %d %d\n", (int16_t)(r0&0xffff), (int16_t)(r0>>16), (int16_t)(r1&0xffff), (int16_t)(r1>>16), (int16_t)(r2&0xffff), (int16_t)(r2>>16), (int16_t)(r3&0xffff), (int16_t)(r3>>16));
-
-			int32_t r4, r5;
+			// int32_t r4, r5;
 			r4 = (int32_t)((int16_t)(r0 >> 16)); //x1
 			r0 = (int32_t)((int16_t)(r0& 0xffff)); //x0
 			r5 = (int32_t)((int16_t)(r1 >> 16)); //x3
 			r1 = (int32_t)((int16_t)(r1& 0xffff)); //x2
-
 
 			//stage 2
 			reflector(r0,r5);
@@ -100,18 +87,27 @@ void dct_round1(register uint8_t x[240][320], register int16_t X[240][320]) {
 			reflector(r5,r2);
 			scale(r4);
 			scale(r3);
-			
-			X[i][0+j] = r0&0xffff;
-			X[i][4+j] = r0>>16;
-			X[i][2+j] = r1&0xffff;
-			X[i][6+j] = r1>>16;
-			X[i][7+j] = r2;
-			X[i][3+j] = r4;
-			X[i][5+j] = r3;
-			X[i][1+j] = r5;
+
+			// X[0+i] = r0&0xffff;
+			// X[1+i] = r5;
+			// X[2+i] = r1&0xffff;
+			// X[3+i] = r4;
+			// X[4+i] = r0>>16;
+			// X[5+i] = r3;
+			// X[6+i] = r1>>16;
+			// X[7+i] = r2;
+			// printf("%d\n", (int16_t)(r0&0xffff));
+			// return;
+			*X = (r5 << 16) | (r0&0xffff);
+			X++;
+			*X = (r4 << 16) | (r1&0xffff);
+			X++;
+			*X = ((r3&0xffff) << 16) | ((r0&0xffff0000)>>16);
+			X++;
+			*X = ((r2&0xffff) << 16) | ((r1&0xffff0000)>>16);
+			X++;
 		}
 	}
-}
 void dct_round2(register int16_t X[240][320]) {
 	register int i;
 	register int j;
@@ -126,7 +122,6 @@ void dct_round2(register int16_t X[240][320]) {
 			register int32_t x6 = X[i+6][j];
 			register int32_t x7 = X[i+7][j];
 
-			register int32_t tmp0;
 			//stage 1
 			reflector(x0,x7);
 			reflector(x1,x6);
@@ -146,15 +141,84 @@ void dct_round2(register int16_t X[240][320]) {
 			reflector(x7,x4);
 			scale(x5);
 			scale(x6);
-			
-			X[i+0][j] = round_3(x0);
-			X[i+4][j] = round_3(x1);
-			X[i+2][j] = round_3(x2);
-			X[i+6][j] = round_3(x3);
-			X[i+7][j] = round_3(x4);
-			X[i+3][j] = round_3(x5);
-			X[i+5][j] = round_3(x6);
-			X[i+1][j] = round_3(x7);
+
+			X[i+0][j] = round_5(x0);
+			X[i+1][j] = round_5(x7);
+			X[i+2][j] = round_5(x2);
+			X[i+3][j] = round_5(x5);
+			X[i+4][j] = round_5(x1);
+			X[i+5][j] = round_5(x6);
+			X[i+6][j] = round_5(x3);
+			X[i+7][j] = round_5(x4);	
+
+			// int32_t r0, r1, r2, r3, r4, r5;
+			// r0 = ((X[i+1][j])<<16)|(X[i+0][j]);
+			// r1 = ((X[i+3][j] & 0xffff)<<16)|(X[i+2][j] & 0xffff);
+			// r2 = ((X[i+5][j] & 0xffff)<<16)|(X[i+4][j] & 0xffff);
+			// r3 = ((X[i+7][j] & 0xffff)<<16)|(X[i+6][j] & 0xffff);
+
+			// printf("%d\n", (int16_t)(r0&0xffff));
+			// return;
+
+			// reflector_1(r0, r3);
+			// reflector_2(r0, r3);
+			// reflector_1(r1, r2);
+			// reflector_2(r1, r2);
+
+			// // int32_t r4, r5;
+			// r4 = (int32_t)((int16_t)(r0 >> 16)); //x1
+			// r0 = (int32_t)((int16_t)(r0& 0xffff)); //x0
+			// r5 = (int32_t)((int16_t)(r1 >> 16)); //x3
+			// r1 = (int32_t)((int16_t)(r1& 0xffff)); //x2
+
+			// //stage 2
+			// reflector(r0,r5);
+			// reflector(r4,r1);
+			// //stage 3
+			// reflector(r0,r4);
+			// rotator(r1,r5,35468,50159,-121095);   //sqrt2_c6
+
+			// r0 &= 0xffff;
+			// r1 &= 0xffff;
+			// r0 |= r4 << 16; //x10
+			// r1 |= r5 << 16; //x32
+
+			// r4 = (int32_t)((int16_t)(r2 >> 16)); //x5
+			// r2 = (int32_t)((int16_t)(r2& 0xffff)); //x4
+			// r5 = (int32_t)((int16_t)(r3 >> 16)); //x7
+			// r3 = (int32_t)((int16_t)(r3& 0xffff)); //x6
+
+
+			// //stage 2
+			// rotator(r2,r5,54491,-18081, -90901);  //c3
+			// rotator(r4,r3,64277,-51491,-77062); //c1
+			// //stage 3
+			// reflector(r2,r3);
+			// reflector(r5,r4);
+			// //stage 4
+			// reflector(r5,r2);
+			// scale(r4);
+			// scale(r3);
+
+			// // X[0+i] = r0&0xffff;
+			// // X[1+i] = r5;
+			// // X[2+i] = r1&0xffff;
+			// // X[3+i] = r4;
+			// // X[4+i] = r0>>16;
+			// // X[5+i] = r3;
+			// // X[6+i] = r1>>16;
+			// // X[7+i] = r2;
+
+			// X[i+0][j] = round_5(r0&0xffff);
+			// X[i+1][j] = round_5(r5);
+			// X[i+2][j] = round_5(r1&0xffff);
+			// X[i+3][j] = round_5(r4);
+			// X[i+4][j] = round_5(r0>>16);
+			// X[i+5][j] = round_5(r3);
+			// X[i+6][j] = round_5(r1>>16);
+			// X[i+7][j] = round_5(r2);	
+
+
 		}
 	}
 }
