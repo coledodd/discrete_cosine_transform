a48619c19dfd346b5ef356b28a5f21ede899f708
coledodd <51397336+coledodd@users.noreply.github.com>
2021-08-17 20:24:23 -0700

    fixed reflector_1 and reflector_2

--- loefflers_dct.c
+++ loefflers_dct.c
@@ -6,8 +6,8 @@
 #define scale(a) a*=92682; a = round_16(a);
 #define round_16(a) ((a & 0x8000) ? ((a>>16) + 1) : (a>>16))
 #define round_3(a) ((a & 0b10000) ? ((a >> 5) + 1) : (a >> 5))
-// #define reflector_1(a,b) { uint32_t tmp = a; a = tmp&0xffff0000 | (tmp&0x0000ffff + b>>16); b = (tmp&0x0000ffff | (tmp&0x0000ffff - b>>16) << 16; }
-// #define reflector_2(a,b) { uint32_t tmp = a; a = tmp&0x0000ffff | (tmp>>16 + b&0x0000ffff);  a = tmp&0x0000ffff | (tmp>>16 - b&0x0000ffff) << 16; }
+// #define reflector_1(a,b) { uint32_t tmp = a; a = tmp&0xffff0000 | (tmp&0x0000ffff + b>>16); b = (b&0x0000ffff | (tmp&0x0000ffff - b>>16) << 16; }
+// #define reflector_2(a,b) { uint32_t tmp = a; a = tmp&0x0000ffff | (tmp>>16 + b&0x0000ffff) << 16;  b = b&0xffff0000 | (tmp>>16 - b&0x0000ffff); }
 
 void dct_round1(register uint8_t x[240][320], register int16_t X[240][320]) {
 	register int i;
